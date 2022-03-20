4eb86ce225d954e36fe836c8e8bee928d5dbe342
ludwig <murimiludwig@gmail.com>
2022-03-20 22:54:02 +0300

    this will add js function

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ function Getpizza( name,size,crust,topping, total ){
   this.topping = topping;
   this.total = total;
 }
+
+$(document).ready(function(){
+  $("button.proceed").click(function(event){
+   let pname = $(".name option:selected").val();
+   let psize = $("#size option:selected").val();
+   let pcrust = $("#crust option:selected").val();
+   let ptopping = [];
+   $.each($("input[name='toppings']:checked"), function(){            
+       ptopping.push($(this).val());
+   });
+   console.log(ptopping.join(", "));
+
+   
