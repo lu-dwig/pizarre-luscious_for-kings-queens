74ee95ae29157d5bfb7cf2e9efeca84a37bd726c
ludwig <murimiludwig@gmail.com>
2022-03-21 13:30:28 +0300

    this will modify some js edit

--- js/script.js
+++ js/script.js
@@ -41,11 +41,18 @@ $("button.addPizza").click(function(){
         default:
           console.log("No price"); 
       }
+      let topping_value = ptopping.length*50;
+        console.log("toppins value" + topping_value);
+        total = price + crust_price + topping_value;
+        console.log(total);
 
+        checkoutTotal = checkoutTotal + total;
+        console.log(checkoutTotal);
+      var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
 
-
-
-
+      $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
+      console.log(newOrder);
+    });
 
 
 var price , crust_price, topping_price ;
